2d3f4a49326ae60b590cc08b09ae069f7140a2ab
Yue Qi <yue.qi.chum@ssss.gouv.qc.ca>
2024-02-29 20:55:56 -0500

    Update DPSynthpop class

--- dpart/engines/dp_synthpop.py
+++ dpart/engines/dp_synthpop.py
@@ -1,14 +1,36 @@
+"""
+This file is under the following license and copyright.
+MIT License
+Copyright (c) 2022 dpart
+
+The following modifications were made to the file:
+    - The differential private synthpop class is modified to include more arguments.
+    - The type hints are updated.
+"""
+
+from typing import Dict, Union, List
 from dpart.dpart import dpart
 from dpart.methods import LogisticRegression, LinearRegression
 
 
-class DPsynthpop(dpart):
+class DPSynthpop(dpart):
     default_numerical = LinearRegression
     default_categorical = LogisticRegression
 
-    def __init__(self,
-                 epsilon: dict = None,
-                 methods: dict = None,
-                 visit_order: list = None,
-                 bounds: dict = None):
-        super().__init__(methods=methods, epsilon=epsilon, visit_order=visit_order, bounds=bounds)
+    def __init__(
+        self,
+        methods: dict = None,
+        epsilon: Union[float, Dict[str, Union[float, Dict[str, float]]]] = None,
+        bounds: Dict[str, List] = None,
+        visit_order: List[str] = None,
+        prediction_matrix: Union[str, Dict[str, List[str]]] = None,
+        n_parents: int = None,
+    ):
+        super().__init__(
+            methods=methods,
+            epsilon=epsilon,
+            bounds=bounds,
+            visit_order=visit_order,
+            prediction_matrix=prediction_matrix,
+            n_parents=n_parents,
+        )
